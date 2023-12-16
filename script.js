edf6d2ebf0231101ae9bd8fe2417ac512417b23f
Kevin <eisermanka@gmail.com>
2023-12-16 18:53:32 -0500

    Added win check for rows

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Gameboard() {
+const Gameboard = (() => {
   const rows = 3;
   const cols = 3;
   const board = [];
@@ -22,15 +22,13 @@ function Gameboard() {
   };
 
   return { getBoard, displayBoard };
-}
+})();
 
 function Cell() {
   let cell = 0;
 
   const setMove = (move) => {
-    if (cell === 0) {
-      cell = move;
-    }
+    cell = move;
   };
 
   const getCell = () => {
@@ -40,11 +38,56 @@ function Cell() {
   return { setMove, getCell };
 }
 
+function Player(id, name) {
+  let playerId = id;
+  let playerName = name;
+
+  const getId = () => playerId;
+  const getName = () => playerName;
+
+  return { getId, getName };
+}
+
 function gameController() {
-  const gameboard = Gameboard();
-  const board = gameboard.getBoard();
-  board[0][0].setMove(2);
-  gameboard.displayBoard();
+  const board = Gameboard.getBoard();
+  const players = [Player(1, "Kevin"), Player(2, "Computer")];
+
+  board[0][0].setMove(1);
+  board[0][1].setMove(1);
+  board[0][2].setMove(1);
+
+  console.log(checkWin(board));
+
+  Gameboard.displayBoard();
+}
+
+function checkWin(board) {
+  const condition1 = (board) => {
+    for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+      const row = board[rowIndex];
+      let firstCell = row[0].getCell();
+      for (let i = 1; i < row.length; i++) {
+        let currentCell = row[i].getCell();
+        if (currentCell === 0) break;
+        if (currentCell !== firstCell) break;
+        if (i === row.length - 1) return true;
+      }
+    }
+  };
+
+  const condition2 = (board) => {
+    return false;
+  };
+
+  const condition3 = (board) => {
+    return false;
+  };
+
+  if (condition1(board) || condition2(board) || condition3(board)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const game = gameController();
